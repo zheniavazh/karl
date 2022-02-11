24844decc1b74b20871cabfd9fdaeaa064943d55
Zhenia Vazhenina <g7806780@gmail.com>
2022-02-11 18:50:21 +0300

    Add backend - good router

--- server/routes/good.routes.js
+++ server/routes/good.routes.js
@@ -13,4 +13,20 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.patch("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (id) {
+            const updatedGood = await Good.findByIdAndUpdate(id, req.body, {
+                new: true,
+            });
+            res.send(updatedGood);
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "На сервере произошла ошибка. Попробуйте позже.",
+        });
+    }
+});
+
 module.exports = router;
